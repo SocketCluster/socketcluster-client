92def9d236e15e9f86e7788286ee44fbfea3b7a9
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2016-08-30 18:14:28 +0300

    v5.0.8 - Fixed bad dependency version issue

--- socketcluster.js
+++ socketcluster.js
@@ -15,7 +15,7 @@ module.exports.destroy = function (options) {
   return SCSocketCreator.destroy(options);
 };
 
-module.exports.version = '5.0.7';
+module.exports.version = '5.0.8';
 
 },{"./lib/scsocket":4,"./lib/scsocketcreator":5,"sc-emitter":14}],2:[function(require,module,exports){
 (function (global){
