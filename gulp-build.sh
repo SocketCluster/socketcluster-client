b7d8dbed4038a0943534f01d9d890fcc72d8cbe4
odenktools <odenktools86@gmail.com>
2017-09-06 11:59:43 +0700

    Remove unused depedencies Optimize output size

--- gulp-build.sh
+++ gulp-build.sh
@@ -5,7 +5,5 @@ echo "Building JavaScript SDK v$SDK_VERSION...\n"
 echo "Cleaning up old builds...\n"
 rm -rf socketcluster.js socketcluster.min.js
 echo "Browser Release:"
-SC_BUILD=browser gulp compile
-echo "Bundling and minifying for CDN distribution:"
 gulp browserify
 gulp minify
--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,21 @@
-var babel = require('gulp-babel');
-var browserify = require('browserify');
-var derequire = require('gulp-derequire');
 var gulp = require('gulp');
-var insert = require('gulp-insert');
 var path = require('path');
+var browserify = require('browserify');
+var babel = require('gulp-babel');
+var derequire = require('gulp-derequire');
+var insert = require('gulp-insert');
 var rename = require('gulp-rename');
-var replace = require('gulp-replace');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
-var umd = require('gulp-umd');
 
 var BUILD = 'browser';
 var DIST = './';
 var VERSION = require('./package.json').version;
 
-var PRESETS = {
-	'browser': ['es2015', 'react', 'stage-2'],
-};
-var PLUGINS = {
-	'browser': ['inline-package-json', 'transform-inline-environment-variables', 'transform-runtime'],
-};
-
-var DEV_HEADER = (
-	'/**\n' +
-	' * SocketCluster JavaScript client v' + VERSION + '\n' +
-	' */\n');
-
 var FULL_HEADER = (
 	'/**\n' +
 	' * SocketCluster JavaScript client v' + VERSION + '\n' +
 	' */\n');
-
-gulp.task('compile', function () {
-	var packageJSON = {
-		version: VERSION
-	};
-	return gulp.src('lib/*.js')
-	.pipe(babel({
-			comments: false,
-			presets: PRESETS[BUILD],
-			plugins: PLUGINS[BUILD],
-		}))
-	// Second pass to kill BUILD-switched code
-	.pipe(babel({
-			plugins: ['minify-dead-code-elimination'],
-		}))
-	.pipe(umd())
-	.pipe(gulp.dest(BUILD));
-});
 
 gulp.task('browserify', function () {
 	var stream = browserify({
@@ -58,17 +26,23 @@ gulp.task('browserify', function () {
 		.ignore('_process')
 		.bundle();
 	return stream.pipe(source('socketcluster.js'))
-		.pipe(derequire())
-		.pipe(insert.prepend(DEV_HEADER))
-		.pipe(gulp.dest(DIST));
+	.pipe(derequire())
+	.pipe(insert.prepend(FULL_HEADER))
+	.pipe(gulp.dest(DIST));
 });
 
 gulp.task('minify', function () {
 	return gulp.src(DIST + 'socketcluster.js')
-		.pipe(uglify())
-		.pipe(insert.prepend(FULL_HEADER))
-		.pipe(rename({
-				extname: '.min.js'
-			}))
-		.pipe(gulp.dest(DIST))
+	.pipe(babel({
+			comments: false
+		}))
+	.pipe(babel({
+			plugins: ["minify-dead-code-elimination"]
+		}))
+	.pipe(uglify())
+	.pipe(insert.prepend(FULL_HEADER))
+	.pipe(rename({
+			extname: '.min.js'
+		}))
+	.pipe(gulp.dest(DIST))
 });
