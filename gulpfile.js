2ea852233d1a9ebf07531d325225413a25bac5fe
Odenktools <odenktools86@gmail.com>
2017-09-11 11:49:58 +0700

    Fix indentation code

--- gulpfile.js
+++ gulpfile.js
@@ -13,36 +13,36 @@ var DIST = './';
 var VERSION = require('./package.json').version;
 
 var FULL_HEADER = (
-	'/**\n' +
-	' * SocketCluster JavaScript client v' + VERSION + '\n' +
-	' */\n');
+  '/**\n' +
+  ' * SocketCluster JavaScript client v' + VERSION + '\n' +
+  ' */\n');
 
-gulp.task('browserify', function () {
-	var stream = browserify({
-			builtins: ['_process', 'events', 'buffer', 'querystring'],
-			entries: 'index.js',
-			standalone: 'socketCluster'
-		})
-		.ignore('_process')
-		.bundle();
-	return stream.pipe(source('socketcluster.js'))
-	.pipe(derequire())
-	.pipe(insert.prepend(FULL_HEADER))
-	.pipe(gulp.dest(DIST));
+gulp.task('browserify', function() {
+  var stream = browserify({
+      builtins: ['_process', 'events', 'buffer', 'querystring'],
+      entries: 'index.js',
+      standalone: 'socketCluster'
+    })
+    .ignore('_process')
+    .bundle();
+  return stream.pipe(source('socketcluster.js'))
+    .pipe(derequire())
+    .pipe(insert.prepend(FULL_HEADER))
+    .pipe(gulp.dest(DIST));
 });
 
-gulp.task('minify', function () {
-	return gulp.src(DIST + 'socketcluster.js')
-	.pipe(babel({
-			comments: false
-		}))
-	.pipe(babel({
-			plugins: ["minify-dead-code-elimination"]
-		}))
-	.pipe(uglify())
-	.pipe(insert.prepend(FULL_HEADER))
-	.pipe(rename({
-			extname: '.min.js'
-		}))
-	.pipe(gulp.dest(DIST))
+gulp.task('minify', function() {
+  return gulp.src(DIST + 'socketcluster.js')
+    .pipe(babel({
+      comments: false
+    }))
+    .pipe(babel({
+      plugins: ["minify-dead-code-elimination"]
+    }))
+    .pipe(uglify())
+    .pipe(insert.prepend(FULL_HEADER))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest(DIST))
 });
