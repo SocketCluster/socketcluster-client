8d011defea19335293c79c9f6749b628e243071c
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2017-11-05 02:03:37 +0000

    v9.0.0

--- socketcluster.js
+++ socketcluster.js
@@ -1,5 +1,5 @@
 /**
- * SocketCluster JavaScript client v8.0.2
+ * SocketCluster JavaScript client v9.0.0
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.socketCluster = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var Emitter = _dereq_('component-emitter');
@@ -257,7 +257,7 @@ module.exports.destroy = function (options) {
 
 module.exports.connections = SCSocketCreator.connections;
 
-module.exports.version = '8.0.2';
+module.exports.version = '9.0.0';
 
 },{"./lib/scsocket":6,"./lib/scsocketcreator":7,"component-emitter":14}],4:[function(_dereq_,module,exports){
 (function (global){
@@ -441,6 +441,7 @@ var SCSocket = function (opts) {
   this._localEvents = {
     'connect': 1,
     'connectAbort': 1,
+    'close': 1,
     'disconnect': 1,
     'message': 1,
     'error': 1,
@@ -1000,6 +1001,7 @@ SCSocket.prototype._onSCClose = function (code, data, openAbort) {
   } else {
     Emitter.prototype.emit.call(self, 'disconnect', code, data);
   }
+  Emitter.prototype.emit.call(self, 'close', code, data);
 
   if (!SCSocket.ignoreStatuses[code]) {
     var failureMessage;
