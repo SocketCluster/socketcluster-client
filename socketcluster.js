4cf77959c34274189c315e5d8acbdd3b57762bda
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-02-09 10:03:52 +0100

    Build

--- socketcluster.js
+++ socketcluster.js
@@ -1,7 +1,7 @@
 /**
  * SocketCluster JavaScript client v9.0.3
  */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.socketCluster = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.socketCluster = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(_dereq_,module,exports){
 var SCSocket = _dereq_('./lib/scsocket');
 var SCSocketCreator = _dereq_('./lib/scsocketcreator');
 
@@ -5319,6 +5319,7 @@ module.exports.decycle = decycle;
 },{"./decycle":20}],22:[function(_dereq_,module,exports){
 (function (global){
 var base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var validJSONStartRegex = /^[ \n\r\t]*[{\[]/;
 
 var arrayBufferToBase64 = function (arraybuffer) {
   var bytes = new Uint8Array(arraybuffer);
@@ -5384,6 +5385,11 @@ module.exports.decode = function (input) {
     return input;
   }
   var message = input.toString();
+
+  // Performance optimization to detect invalid JSON packet sooner.
+  if (!validJSONStartRegex.test(message)) {
+    return message;
+  }
 
   try {
     return JSON.parse(message);
