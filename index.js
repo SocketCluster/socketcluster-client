c693d595ad6ac080d2b154150eea2e5833df7abd
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-04-17 00:24:17 +0200

    v11.0.2

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ module.exports.destroy = function (socket) {
 
 module.exports.clients = SCSocketCreator.clients;
 
-module.exports.version = '11.0.1';
+module.exports.version = '11.0.2';
--- socketcluster.js
+++ socketcluster.js
@@ -1,5 +1,5 @@
 /**
- * SocketCluster JavaScript client v11.0.1
+ * SocketCluster JavaScript client v11.0.2
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.socketCluster = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(_dereq_,module,exports){
 var SCSocket = _dereq_('./lib/scsocket');
@@ -22,7 +22,7 @@ module.exports.destroy = function (socket) {
 
 module.exports.clients = SCSocketCreator.clients;
 
-module.exports.version = '11.0.1';
+module.exports.version = '11.0.2';
 
 },{"./lib/scsocket":4,"./lib/scsocketcreator":5,"component-emitter":12}],2:[function(_dereq_,module,exports){
 (function (global){
@@ -1076,9 +1076,12 @@ SCSocket.prototype.channel = function (channelName, options) {
 
 SCSocket.prototype.destroyChannel = function (channelName) {
   var channel = this.channels[channelName];
-  channel.unwatch();
-  channel.unsubscribe();
-  delete this.channels[channelName];
+
+  if (channel) {
+    channel.unwatch();
+    channel.unsubscribe();
+    delete this.channels[channelName];
+  }
 };
 
 SCSocket.prototype.subscriptions = function (includePending) {
