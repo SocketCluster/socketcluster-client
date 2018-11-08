48cd03be8a29f0c4249f8e0bd664c6955433efe2
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-11-07 23:33:27 -0300

    Fix gulp uglify so that it supports ES6

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,25 @@ var gulp = require('gulp');
 var path = require('path');
 var browserify = require('browserify');
 var babel = require('gulp-babel');
-var derequire = require('gulp-derequire');
 var insert = require('gulp-insert');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
+var uglify = require('uglify-es');
+var uglifyComposer = require('gulp-uglify/composer');
+var minify = uglifyComposer(uglify, console);
 var convertNewline = require('gulp-convert-newline');
 
 var BUILD = 'browser';
 var DIST = './';
 var VERSION = require('./package.json').version;
 
+// Add comment. Also, prepend the var keyword to the require variable
+// declaration added by browserify.
 var FULL_HEADER = (
   '/**\n' +
   ' * SocketCluster JavaScript client v' + VERSION + '\n' +
-  ' */\n');
+  ' */\n '
+);
 
 gulp.task('browserify', function (done) {
   var stream = browserify({
@@ -25,14 +29,14 @@ gulp.task('browserify', function (done) {
       standalone: 'socketCluster'
     })
     .ignore('_process')
+    .require('./index.js', {expose: 'socketcluster-client'})
     .bundle();
   return stream.pipe(source('socketcluster.js'))
+    .pipe(insert.prepend(FULL_HEADER))
     .pipe(convertNewline({
       newline: 'lf',
       encoding: 'utf8'
     }))
-    .pipe(insert.prepend(FULL_HEADER))
-    .pipe(derequire())
     .pipe(gulp.dest(DIST));
 });
 
@@ -44,7 +48,7 @@ gulp.task('minify', function () {
     .pipe(babel({
       plugins: ['minify-dead-code-elimination']
     }))
-    .pipe(uglify())
+    .pipe(minify())
     .pipe(insert.prepend(FULL_HEADER))
     .pipe(rename({
       extname: '.min.js'
