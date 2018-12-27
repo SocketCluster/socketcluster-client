e08c3f063e97bb2638e72f44d8077da93f0a0df4
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-12-27 02:53:07 +0100

    Rename

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const ASClientSocket = require('./lib/asclientsocket');
+const AGClientSocket = require('./lib/agclientsocket');
 const factory = require('./lib/factory');
 const version = '1.0.1';
 
 module.exports.factory = factory;
-module.exports.ASClientSocket = ASClientSocket;
+module.exports.AGClientSocket = AGClientSocket;
 
 module.exports.create = function (options) {
   return factory.create({...options, version});
