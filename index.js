d138b9296e5718c7bba7968d8de2e66322409575
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-03-20 23:06:16 +0100

    v5.4.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const AGClientSocket = require('./lib/clientsocket');
 const factory = require('./lib/factory');
-const version = '5.4.0';
+const version = '5.4.1';
 
 module.exports.factory = factory;
 module.exports.AGClientSocket = AGClientSocket;
