69fb145a62672b584272710cf991d1b684b98e22
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2023-07-06 21:36:32 +1000

    v17.2.0 - Fix bundling issue with some frameworks

--- socketcluster-client.js
+++ socketcluster-client.js
@@ -1,5 +1,5 @@
 /**
- * SocketCluster JavaScript client v17.1.1
+ * SocketCluster JavaScript client v17.2.0
  */
  (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.socketClusterClient = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){(function (){
@@ -1632,16 +1632,18 @@ module.exports = {
 (function (global){(function (){
 const AGRequest = require('ag-request');
 
-let WebSocket;
 let createWebSocket;
 
-if (global.WebSocket) {
-  WebSocket = global.WebSocket;
+if (typeof WebSocket !== 'undefined') {
   createWebSocket = function (uri, options) {
     return new WebSocket(uri);
   };
+} else if (typeof global !== 'undefined' && global.WebSocket) {
+  createWebSocket = function (uri, options) {
+    return new global.WebSocket(uri);
+  };
 } else {
-  WebSocket = require('ws');
+  let WebSocket = require('ws');
   createWebSocket = function (uri, options) {
     return new WebSocket(uri, [], options);
   };
@@ -8898,7 +8900,7 @@ module.exports = WritableConsumableStream;
 },{"./consumer":42,"consumable-stream":14}],"socketcluster-client":[function(require,module,exports){
 const AGClientSocket = require('./lib/clientsocket');
 const factory = require('./lib/factory');
-const version = '17.1.1';
+const version = '17.2.0';
 
 module.exports.factory = factory;
 module.exports.AGClientSocket = AGClientSocket;
