87d5a59ae5962293dc49f9950168e54546af42e8
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2023-07-06 23:05:36 +1000

    v17.2.1 - Remove global references where possible

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const AGClientSocket = require('./lib/clientsocket');
 const factory = require('./lib/factory');
-const version = '17.2.0';
+const version = '17.2.1';
 
 module.exports.factory = factory;
 module.exports.AGClientSocket = AGClientSocket;
