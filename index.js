bfca6a109df5a527f8ecd4dba12d75b4dc7a0cd3
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2024-04-02 20:44:05 +1100

    v19.1.2 - Bump version number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const AGClientSocket = require('./lib/clientsocket');
 const factory = require('./lib/factory');
-const version = '17.2.1';
+const version = '19.1.2';
 
 module.exports.factory = factory;
 module.exports.AGClientSocket = AGClientSocket;
