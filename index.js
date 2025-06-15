43f65e20c5a2fbe804a43bbc1c28e9eec46410da
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2025-06-15 21:35:32 +1000

    Fix version number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const AGClientSocket = require('./lib/clientsocket');
 const factory = require('./lib/factory');
-const version = '19.1.2';
+const version = '19.1.4';
 
 module.exports.factory = factory;
 module.exports.AGClientSocket = AGClientSocket;
